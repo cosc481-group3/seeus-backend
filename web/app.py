253f0cf5167392c04aa04e171ccb2b80a1a16b03
Nathan Johnson <n.johnson@criteo.com>
2020-03-28 16:33:20 -0400

    Set up Pipenv and docker-compose
    * Flask and Postgres running through docker-compose
    * Pipenv managing dependencies
    * Example endpoints in app.py

---
+++ web/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+
+app = Flask('SEEUS')
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/hello/<name>')
+def test(name):
+    out = {
+        "message": "Hello, " + name,
+        "query_params": request.args,
+        "name": name
+    }
+    return jsonify(out)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
