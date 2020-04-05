1d0c931881ff4836adc4a091b82c0383ae3faeb9
Mazawrath <Mazawrath@users.noreply.github.com>
2020-04-04 23:35:14 -0400

    Added connection to database

---
+++ src/database.py
@@ -0,0 +1,10 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+try:
+    conn = psycopg2.connect(f"dbname='postgres' user='{os.getenv('POSTGRES_USER')}' host='database' password='{os.getenv('POSTGRES_PASSWORD')}'")
+except:
+    print("Unable to connect to the database")
