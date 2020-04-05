684fc734ddb841c2c1e1527b25c08396388bbcf4
Mazawrath <Mazawrath@users.noreply.github.com>
2020-04-05 00:57:54 -0400

    Added env variables

--- src/database.py
+++ src/database.py
@@ -5,6 +5,6 @@ import os
 load_dotenv()
 
 try:
-    conn = psycopg2.connect(f"dbname='postgres' user='{os.getenv('POSTGRES_USER')}' host='database' password='{os.getenv('POSTGRES_PASSWORD')}'")
+    conn = psycopg2.connect(f"dbname='{os.getenv('POSTGRES_DB')}' user='{os.getenv('POSTGRES_USER')}' host='{os.getenv('POSTGRES_USER')}' password='{os.getenv('POSTGRES_PASSWORD')}'")
 except psycopg2.Error:
     print("Unable to connect to the database")
