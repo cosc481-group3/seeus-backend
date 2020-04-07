e8a598b5878896afe638f9aee812ab25e1dff077
Nathan Johnson <n.johnson@criteo.com>
2020-04-07 19:11:49 -0400

    Fix config object issue

--- src/config.py
+++ src/config.py
@@ -1,18 +1,25 @@
-from dotenv import load_dotenv
 from os import getenv
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
-config = {
-    'mode': getenv('FLASK_ENV'),
-    'database': {
-        'host': getenv('POSTGRES_HOST'),
-        'user': getenv('POSTGRES_USER'),
-        'password': getenv('POSTGRES_PASSWORD'),
-        'name': getenv('POSTGRES_DB')
-    },
-    'oauth': {
-        'client_id': getenv('OAUTH_CLIENT_ID'),
-        'client_secret': getenv('OAUTH_CLIENT_SECRET'),
-    }
-}
+
+class Config:
+    class DatabaseConfig:
+        host = getenv('POSTGRES_HOST')
+        user = getenv('POSTGRES_USER')
+        password = getenv('POSTGRES_PASSWORD')
+        name = getenv('POSTGRES_DB')
+
+    class OauthConfig:
+        client_id = getenv('OAUTH_CLIENT_ID')
+        client_secret = getenv('OAUTH_CLIENT_SECRET')
+
+    mode = getenv('FLASK_ENV')
+
+    database = DatabaseConfig()
+    oauth = OauthConfig()
+
+
+config = Config()
