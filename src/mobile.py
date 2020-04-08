2dbab0bd9e1d505f22dd5cec8cd4548262372a01
Mazawrath <Mazawrath@users.noreply.github.com>
2020-04-07 23:40:08 -0400

    Setup requests to work

--- src/mobile.py
+++ src/mobile.py
@@ -1,6 +1,5 @@
 from flask import jsonify, request, Blueprint
 from random import random
-
 from database import conn
 
 mobile = Blueprint('app', __name__, url_prefix='/app')
@@ -17,6 +16,24 @@ def hello():
     cursor = conn.cursor()
     cursor.execute("select * from seeus_config")
     return jsonify(cursor.fetchall())
+
+
+@mobile.route('/requests', methods=['POST'])
+def requests():
+    data = request.get_json(force=True)
+    conn.cursor().execute("insert into requests (user_id, start_location, end_location, start_latitude, "
+                          "start_longitude, notes) "
+                          "values (%(user_id)s, %(start_location)s, %(end_location)s, %(start_latitude)s, "
+                          "%(start_longitude)s, %(notes)s)", {
+                              'user_id': 1,
+                              'start_location': data["start_location"],
+                              'end_location': data["end_location"],
+                              'start_latitude': data["start_latitude"],
+                              'start_longitude': data["start_longitude"],
+                              'notes': data["notes"],
+                          })
+    conn.commit()
+    return data
 
 
 @mobile.route('/requests/scheduled')
