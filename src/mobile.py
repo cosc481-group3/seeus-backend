8244b1a331fa6e7f512af02f87e3eb47df22074e
Mazawrath <Mazawrath@users.noreply.github.com>
2020-04-07 23:42:28 -0400

    Removed test code

--- src/mobile.py
+++ src/mobile.py
@@ -1,21 +1,7 @@
 from flask import jsonify, request, Blueprint
-from random import random
 from database import conn
 
 mobile = Blueprint('app', __name__, url_prefix='/app')
-
-
-@mobile.route('/')
-def hello():
-    conn.cursor().execute("insert into seeus_config (name, value) "
-                          "values (%(name)s, %(value)s)", {
-                              'name': f'name{round(random() * 1000)}',
-                              'value': 'some value'
-                          })
-    conn.commit()
-    cursor = conn.cursor()
-    cursor.execute("select * from seeus_config")
-    return jsonify(cursor.fetchall())
 
 
 @mobile.route('/requests', methods=['POST'])
