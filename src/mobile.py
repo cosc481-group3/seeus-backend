097fd0d4089b80ee5eebc067e3edbaa63f746341
Mazawrath <Mazawrath@users.noreply.github.com>
2020-04-07 22:44:51 -0400

    Formatted file

--- src/mobile.py
+++ src/mobile.py
@@ -3,20 +3,21 @@ from random import random
 
 from database import conn
 
-
 mobile = Blueprint('app', __name__, url_prefix='/app')
+
 
 @mobile.route('/')
 def hello():
     conn.cursor().execute("insert into seeus_config (name, value) "
                           "values (%(name)s, %(value)s)", {
-        'name': f'name{round(random()*1000)}',
-        'value': 'some value'
-    })
+                              'name': f'name{round(random() * 1000)}',
+                              'value': 'some value'
+                          })
     conn.commit()
     cursor = conn.cursor()
     cursor.execute("select * from seeus_config")
     return jsonify(cursor.fetchall())
+
 
 @mobile.route('/requests/scheduled')
 def test(name):
